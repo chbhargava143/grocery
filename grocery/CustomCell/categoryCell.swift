4e5494723f1aee3d29cde76ff424fbd7b31c5f3b
bhargava <chbhargava4@gmail.com>
2021-02-04 15:39:46 +0530

    images Added

---
+++ grocery/CustomCell/categoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  categoryCell.swift
+//  grocery
+//
+//  Created by bhargava on 04/02/21.
+//
+
+import UIKit
+
+class categoryCell: UICollectionViewCell {
+    @IBOutlet weak var categoryImage:UIImageView!
+    @IBOutlet weak var categoryNameLbl:UILabel!
+}
---
+++ grocery/Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  grocery
+//
+//  Created by bhargava on 04/02/21.
+//
+
+import Foundation
+
+// Firebase Headers
+public let KUSER_PATH = "User"
+public let KCATEGORY_PATH = "Category"
+public let KITEMS_PATH = "Items"
+public let KBASKET_PATH = "Basket"
+
+// Category
+public let KNAME = "name"
+public let KIMAGENAME = "imageName"
+public let KOBJECTID = "objectId"
